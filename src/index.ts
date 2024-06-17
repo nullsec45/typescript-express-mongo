222925f60d503ba3243a5189d6753c711449e601
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-17 14:42:16 +0700

    feat:setup typescript, nodemon, express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {Application, Request, Response, NextFunction} from "express";
+
+const app: Application=express();
+const port:Number=4000;
+
+app.use("/", (req:Request, res:Response, next:NextFunction) => {
+    res.status(200).send({data:"Hello World"});
+});
+app.listen(port, () => console.log(`Server berjalan pada port ${port}`));
