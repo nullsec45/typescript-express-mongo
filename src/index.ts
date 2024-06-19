6f387f8bc1d4670bef2df7fddab7ce256759c51c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-19 22:47:22 +0700

    formatter standard eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { type Application, type Request, type Response, type NextFunction } from 'express';
 
 const app: Application = express();
-const port: Number = 4000;
+const port: number = 4000;
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'Hello World' });
 });
 
-app.listen(port, () => console.log(`Server berjalan pada port ${port}`));
+app.listen(port, () => {
+  console.log(`Server berjalan pada port ${port}`);
+});
