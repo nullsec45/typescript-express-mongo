a7b86bb5dd98ae8358b7fa79d6eb8268c98696d6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-19 22:42:00 +0700

    chore:Setup Eslint,Prettier,Husky

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ const port: Number = 4000;
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'Hello World' });
 });
+
 app.listen(port, () => console.log(`Server berjalan pada port ${port}`));
