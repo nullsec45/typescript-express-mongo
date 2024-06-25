2181889dfa3f1333c9c25c05ad420f23d1ca9e16
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-25 07:55:10 +0700

    feat : validation error handling product route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,4 +1,5 @@
 import { Router, type Request, type Response, type NextFunction } from 'express';
+import { createProductValidation } from '../validations/product.validation';
 import { logger } from '../utils/logger';
 
 export const ProductRouter: Router = Router();
@@ -6,7 +7,7 @@ export const ProductRouter: Router = Router();
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('access get route product');
 
-  res.status(200).send({
+  return res.status(200).send({
     stattus: true,
     statusCode: 200,
     data: [
@@ -22,9 +23,19 @@ ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('access post route product');
   logger.info(`Request body : ${JSON.stringify(req.body)}`);
 
-  res.status(200).send({
+  const { error } = createProductValidation(req.body);
+
+  if (error) {
+    logger.error('ERR : product - create', error.details[0].message);
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message });
+  }
+
+  logger.info('Success add new product');
+
+  return res.status(200).send({
     stattus: true,
     statusCode: 200,
+    message: 'Add product sucess',
     data: req.body
   });
 });
