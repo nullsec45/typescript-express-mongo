a2a0d3f31af353e07e1283296755d9ff266f9504
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-29 23:14:32 +0700

    feat: connect db to index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { logger } from './utils/logger';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+// connect to db
+import './utils/connectDB';
+
 const app: Application = express();
 const port: number = 4000;
 
