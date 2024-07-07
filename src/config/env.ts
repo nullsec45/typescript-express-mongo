bbe3989b5641708c274a29ccca27d87852effa4f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-07 17:55:36 +0700

    feat: env jwt public & private

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, JWT_PUBLIC, JWT_PRIVATE } = process.env;
 
 const CONFIG = {
-  db: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+  db: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+  jwt_public: JWT_PUBLIC,
+  jwt_private: JWT_PRIVATE
 };
 
 export default CONFIG;
