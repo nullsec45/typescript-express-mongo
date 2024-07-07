f56589a876c4250f5b4364451d263b3a48ac54dd
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-07 17:51:58 +0700

    feat : create method checkPassword for compare password

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -3,3 +3,8 @@ import bcrypt from 'bcrypt';
 export const hashing = (password: string) => {
   return bcrypt.hashSync(password, 10);
 };
+
+// checking password
+export const checkPassword = (password: string, userPassword: string) => {
+  return bcrypt.compareSync(password, userPassword);
+};
