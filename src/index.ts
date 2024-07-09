7108878d482634a1d918c965f33fe50605db71e5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-09 22:03:32 +0700

    feat : move app express to utils server

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,11 @@
-import express, { type Application, type Response } from 'express';
-import { routes } from './routes';
 import { logger } from './utils/logger';
-import bodyParser from 'body-parser';
-import cors from 'cors';
 
 // connect to db
 import './utils/connectDB';
+import createServer from './utils/server';
 
-// middleware jwt
-import deserializedToken from './middleware/deserialized';
-
-const app: Application = express();
+const app = createServer();
 const port: number = 4000;
-
-// parse body request
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(deserializedToken);
-
-routes(app);
-
-// cors access handler
-app.use(cors());
-app.use((req, res: Response, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-});
 
 app.listen(port, () => {
   logger.info(`Server berjalan pada port ${port}`);
