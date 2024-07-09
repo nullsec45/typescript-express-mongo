eb2884ae8ebb5036f92b5cca28d3e6a73bbe8cfe
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-09 22:03:01 +0700

    feat : jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+.tsx?$': ['ts-jest', {}]
+  },
+  testMatch: ['**/**/*.test.ts'],
+  verbose: true,
+  forceExit: true
+};
